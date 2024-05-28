00c0f4e94e78f60a47651ba5507a5819a25ea031
rfsyhb <rafisyihab3@gmail.com>
2024-05-28 17:24:44 +0700

    coba handling

--- src/server.js
+++ src/server.js
@@ -15,8 +15,29 @@ const init = async () => {
 
   server.route(routes);
 
+  // Event handler for client errors
+  server.listener.on('clientError', (err, socket) => {
+    console.error('Client error:', err);
+    socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+  });
+
+  // Event handler for errors on the socket
+  server.listener.on('error', (err, socket) => {
+    console.error('Socket error:', err);
+    socket.destroy();
+  });
+
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
+
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+process.on('uncaughtException', (err) => {
+  console.log('Uncaught Exception:', err);
+});
 
 init();
